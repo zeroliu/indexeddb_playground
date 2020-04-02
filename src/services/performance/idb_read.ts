a6b85f027d57e0b6081eb33451f2497b5a1d6d77
Zero Liu <zerolxy@gmail.com>
2020-04-02 11:39:04 -0700

    Added idb read with getAll

--- src/services/performance/idb_read.ts
+++ src/services/performance/idb_read.ts
@@ -51,6 +51,35 @@ function cleanup() {
   });
 }
 
+function benchmarkReadGetAll() {
+  return new Promise<number>((resolve, reject) => {
+    const results: Record<string, {}> = {};
+    const request = indexedDB.open('idb-playground-benchmark', 1);
+
+    request.onsuccess = e => {
+      const db = (e.target as any).result as IDBDatabase;
+      const start = performance.now();
+      const transaction = db.transaction('entries', 'readonly');
+      const store = transaction.objectStore('entries');
+      const getAllRequest = store.getAll();
+      getAllRequest.onsuccess = e => {
+        const items = (e.target as any).result;
+        items.forEach((item: {key: string; blob: string}) => {
+          results[item.key] = item.blob;
+        });
+        const end = performance.now();
+        db.close();
+        resolve(end - start);
+      };
+      getAllRequest.onerror = e => {
+        const msg = (e.target as any).error.message;
+        logError(msg, 'idb_read');
+        reject(msg);
+      };
+    };
+  });
+}
+
 function benchmarkReadCursor() {
   return new Promise<number>((resolve, reject) => {
     const results: Record<string, {}> = {};
@@ -86,7 +115,46 @@ const baseCase = {
   // idb tests are really slow. Only run 100 iterations.
   iteration: 100,
   cleanup,
-  benchmark: () => benchmarkReadCursor(),
+};
+
+const getAllBaseCase = {
+  ...baseCase,
+  benchmark: () => benchmarkReadGetAll(),
+};
+
+const read1000x100BGetAll: PerformanceTestCase = {
+  ...getAllBaseCase,
+  name: 'idbRead1000x100BGetAll',
+  label: 'idb read 1000x100B with getAll',
+  prep: () => prep(1000, generateString(0.1)),
+};
+
+const read10000x100BGetAll: PerformanceTestCase = {
+  ...getAllBaseCase,
+  name: 'idbRead10000x100BGetAll',
+  label: 'idb read 10000x100B with getAll',
+  prep: () => prep(10000, generateString(0.1)),
+};
+
+const read100x500BGetAll: PerformanceTestCase = {
+  ...getAllBaseCase,
+  name: 'idbRead100x500BGetAll',
+  label: 'idb read 100x500B with getAll',
+  prep: () => prep(100, generateString(0.5)),
+};
+
+const read100x1KBGetAll: PerformanceTestCase = {
+  ...getAllBaseCase,
+  name: 'idbRead100x1KBGetAll',
+  label: 'idb read 100x1KB with getAll',
+  prep: () => prep(100, generateString(1)),
+};
+
+const read100x5KBGetAll: PerformanceTestCase = {
+  ...getAllBaseCase,
+  name: 'idbRead100x5KBGetAll',
+  label: 'idb read 100x5KB with getAll',
+  prep: () => prep(100, generateString(5)),
 };
 
 const cursorBaseCase = {
@@ -130,6 +198,11 @@ const read100x5KBCursor: PerformanceTestCase = {
 };
 
 export const idbReadTestCases = [
+  read1000x100BGetAll,
+  read10000x100BGetAll,
+  read100x500BGetAll,
+  read100x1KBGetAll,
+  read100x5KBGetAll,
   read1000x100BCursor,
   read10000x100BCursor,
   read100x500BCursor,
