1816c6730d0f7d6e15b6accd1d92adade34ef95f
Zero Liu <zerolxy@gmail.com>
2020-04-07 17:03:38 -0700

    Fixed merge conflict

--- src/services/abusers/idb_abuser.ts
+++ src/services/abusers/idb_abuser.ts
@@ -1,14 +1,6 @@
-<<<<<<< Updated upstream
-import { Benchmark } from 'services/benchmark';
-import { logError, log } from 'services/logger';
-import { BaseAbuser } from './base_abuser';
-import { generateString } from 'services/mock_data';
-=======
 import {Benchmark} from 'services/benchmark';
 import {log, logError} from 'services/logger';
 import {generateString} from 'services/mock_data';
->>>>>>> Stashed changes
-
 import {BaseAbuser} from './base_abuser';
 
 const DB_NAME = 'idb_playground_db';
@@ -74,7 +66,7 @@ export class IdbAbuser extends BaseAbuser {
         return new Promise((resolve, reject) => {
           const request = storage.freeSpace();
           request.onsuccess = () => {
-            resolve({ quota: request.result });
+            resolve({quota: request.result});
           };
           request.onerror = () => {
             reject(request.error);
@@ -112,12 +104,12 @@ export class IdbAbuser extends BaseAbuser {
     const content = generateString(sizeInKb);
     const benchmarkCreateObj = new Benchmark('Creating idb objects');
     const benchmarkAddToIdb = new Benchmark(
-      `Adding ${quantity} x ${sizeInKb}kb entries to idb`,
+        `Adding ${quantity} x ${sizeInKb}kb entries to idb`,
     );
     const transaction = this.db!.transaction(ABUSER_STORE, 'readwrite');
     const store = transaction.objectStore(ABUSER_STORE);
     for (let i = 0; i < quantity; ++i) {
-      const blob = new Blob([content], { type: 'text/plain' });
+      const blob = new Blob([content], {type: 'text/plain'});
       const request = store.add(blob);
       request.onerror = () => {
         logError(request.error!.message, 'idb');
