41602790e4bc0d9570d0d4ddb236ff54a3ca8a23
Zero Liu <zerolxy@gmail.com>
2020-08-03 17:13:26 -0700

    Enhanced error message

--- src/services/error.ts
+++ src/services/error.ts
@@ -1,12 +1,20 @@
-import {logError} from './logger';
+import { logError } from './logger';
 
 export function handleError(
   err: DOMException | Error,
   context: string,
-  reject?: (err: Error) => void
+  reject?: (err: Error) => void,
 ) {
-  logError(err.message, context);
-  console.error(err);
+  if ('message' in err) {
+    logError(err.message, context);
+    console.error(err.message);
+  }
+  if ('code' in err) {
+    console.error(err.code);
+  }
+  if ('name' in err) {
+    console.error(err.name);
+  }
   if (reject) {
     reject(err);
   }
